00f4d24cb7400873026dcec2a10a6e8d574d82d0
gemadam <gemadam3@gmail.com>
2021-12-02 11:28:36 +0100

    Working GUI project

--- src/main/java/gembala/adam/caesar/Main.java
+++ src/main/java/gembala/adam/caesar/Main.java
@@ -5,14 +5,52 @@
 package gembala.adam.caesar;
 
 import gembala.adam.cesar.controller.CaesarCipherController;
-import gembala.adam.cesar.model.CaesarCipher;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /**
  * Main class of a program
  * @author Adam Gembala
  * @version 1.0.0
  */
-public class Main {
+public class Main extends Application {
+    
+    /**
+     * Arguments passed by command line
+     */
+    static String[] _args;
+    
+    /**
+     * 
+     * @param primaryStage 
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        
+        Button btn = new Button();
+        btn.setText("Say 'Hello World!' on default Java console");
+        btn.setOnAction((ActionEvent event) -> {
+            // Initialize and execute controller
+            var c = new CaesarCipherController();
+            
+            c.execute(_args);
+            
+            System.out.println("Dupa dupa");
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Dupa dupa");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
     /**
      * Main method of a program
@@ -20,8 +58,8 @@ public class Main {
      */
     public static void main(String args[]) {
         
-        // Initialize and execute controller
-        var c = new CaesarCipherController();
-        c.execute(args);
+        _args = args;
+        
+        launch(args);
     }
 }
